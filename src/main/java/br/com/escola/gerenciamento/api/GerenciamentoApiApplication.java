4782f26b4328476723723f2d08c8650d7c8dcfeb
C1262322 Gilnei Aquino da Paixao <c1262322@interno.bb.com.br>
2021-10-19 10:38:56 -0300

    Primeira versao do projeto criado pelo initlizer do spring boot

---
+++ src/main/java/br/com/escola/gerenciamento/api/GerenciamentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.escola.gerenciamento.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/escola/gerenciamento/api/GerenciamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escola.gerenciamento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
