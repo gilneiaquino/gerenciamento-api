b906bb34fb611731b9d976a24a5b22bb0c32fdab
C1262322 Gilnei Aquino da Paixao <c1262322@interno.bb.com.br>
2021-11-16 00:30:28 -0300

    colcando data e sexo na listagem de alunos

--- src/main/java/br/com/escola/gerenciamento/aluno/controller/dto/AlunoDto.java
+++ src/main/java/br/com/escola/gerenciamento/aluno/controller/dto/AlunoDto.java
@@ -1,20 +1,26 @@
 package br.com.escola.gerenciamento.aluno.controller.dto;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import br.com.escola.gerenciamento.aluno.enums.SEXO;
 import br.com.escola.gerenciamento.aluno.modelo.Aluno;
 
 public class AlunoDto {
 
+	private Long id;
+	private String nome;
+	private LocalDate dataNascimento;
+	private SEXO sexo;
+	
 	public AlunoDto(Aluno aluno) {
 		this.nome = aluno.getNome();
 		this.id = aluno.getId();
+		this.sexo = aluno.getSexo();
+		this.dataNascimento = aluno.getDataNascimento();
 	}
-
-	private Long id;
-	private String nome;
-	
+ 
 	public Long getId() {
 		return id;
 	}
@@ -29,6 +35,22 @@ public class AlunoDto {
 	}
 	public static List<AlunoDto> converter(List<Aluno> alunos) { 
 		return alunos.stream().map(AlunoDto::new).collect(Collectors.toList());
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public SEXO getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(SEXO sexo) {
+		this.sexo = sexo;
 	}
 	
 }
